ab04ed68ae2898d8d09a30e6c978e1a71e3b88a0
NMSaiTharun <nmsaitarun@gmail.com>
2021-12-30 15:36:52 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Algorithms {
+    class Program {
+        static void RotateArrayLeft(int[] input) {
+            int t = input[0];
+
+            for (int i = 0; i < input.Length - 1; i++) {
+                input[i] = input[i + 1];
+            }
+
+           
+
+            input[input.Length - 1] = t;
+        }
+
+        static void RotateArrayRight(int[] input) {
+
+            int t = input[input.Length - 1];
+
+            for (int i = input.Length - 1; i > 0; i--) {
+                input[i] = input[i - 1];
+            }
+
+           
+            input[0] = t;
+        }
+
+        static void Main(string[] args) {
+            int[] arr = { 2,3,4,5,6,1 };
+            RotateArrayLeft(arr); //  array will rotate towards left
+            RotateArrayRight(arr); // array will rotate towards right
+            Array.ForEach(arr, Console.WriteLine);
+        }
+    }
+}
